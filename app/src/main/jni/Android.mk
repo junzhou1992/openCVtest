7fff2541041e9ba438f4506cb97baa04cc3c95b8
junzhou1992 <2014junz@sina.com>
2018-01-27 17:47:24 +0800

    v1,运行人脸检测demo

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+OPENCV_INSTALL_MODULES:=off
+OPENCV_LIB_TYPE:=SHARED
+
+
+ include D:\AndroidStudioProjects\app\OpenCV-android-sdk\sdk\native\jni\OpenCV.mk
+
+
+LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -llog -ldl
+
+LOCAL_MODULE     := detection_based_tracker
+
+include $(BUILD_SHARED_LIBRARY)
